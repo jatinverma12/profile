71ef16342812b70558abbbabf721abe031e2666f
jatin-verma-1999 <jatin71_soe@jnu.ac.in>
2021-05-07 08:56:57 +0530

    fs

--- index.js
+++ index.js
@@ -1,50 +1,9 @@
+// $('.card').click(function(){
+//   $('.card').addClass('cardclicked');
+// })
 
-    $('.minus-btn').on('click', function (e) {
-      e.preventDefault();
-      var $this = $(this);
-      var $input = $this.closest('div').find('input');
-      var value = parseInt($input.val());
-
-      if (value > 1) {
-        value = value - 1;
-      }
-      else {
-        value = 0;
-      }
-
-      $input.val(value);
-    });
-
-    $('.plus-btn').on('click', function (e) {
-      e.preventDefault();
-      var $this = $(this);
-      var $input = $this.closest('div').find('input');
-      var value = parseInt($input.val());
-
-      if (value < 100) {
-        value = value + 1;
-      } else {
-        value = 100;
-      }
-
-      $input.val(value);
-    });
-
-    function openNav() {
-      document.getElementById("mySidepanel").style.width = "500px";
-    }
-
-    function closeNav() {
-      document.getElementById("mySidepanel").style.width = "0";
-    }
-
-    $(document).ready(function(){
-      $("#hideLogin").click(function(){
-        $(".login").hide();
-        $(".gethelp").show();
-      });
-      $("#hideGetHelp").click(function(){
-        $(".gethelp").hide();
-        $(".login").show();
-      });
-    });
+var cards=document.getElementsByClassName('card');
+for(var i=0;i<cards.length;i++)
+  cards[i].addEventListener('click',function(){
+    this.className='cardclicked';
+  })
